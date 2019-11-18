dce05fd7d3bcc0f10b5226196cd26076c40abd92
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2019-11-18 13:52:41 +0700

    hold on keyboard broke

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hakuba/Sites/sdk-dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hakuba/Sites/Android-state/beng"
+export "FLUTTER_TARGET=/Users/hakuba/Sites/Android-state/beng/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hakuba/Sites/sdk-dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
--- lib/components/Product.dart
+++ lib/components/Product.dart
@@ -8,31 +8,31 @@ class Product extends StatefulWidget {
 class _ProductState extends State<Product> {
   var product_list = [
     {
-      "name": "Blazer" ,
+      "name": "Blazer",
       "picture": "images/products/blazer1.jpeg",
       "old_price": 120000,
       "price": 85000
     },
     {
-      "name": "Red Blazer" ,
+      "name": "Red Blazer",
       "picture": "images/products/blazer2.jpeg",
       "old_price": 100000,
       "price": 55000
     },
     {
-      "name": "Dress" ,
+      "name": "Dress",
       "picture": "images/products/dress1.jpeg",
       "old_price": 120000,
       "price": 85000
     },
     {
-      "name": "Dress 2" ,
+      "name": "Dress 2",
       "picture": "images/products/dress2.jpeg",
       "old_price": 120000,
       "price": 85000
     },
     {
-      "name": "Hill" ,
+      "name": "Hill",
       "picture": "images/products/hills1.jpeg",
       "old_price": 120,
       "price": 85
@@ -79,14 +79,14 @@ class _ProductState extends State<Product> {
   Widget build(BuildContext context) {
     return GridView.builder(
       itemCount: product_list.length,
-      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (BuildContext context, int index){
+      gridDelegate:
+          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index) {
         return SingleProduct(
-          prod_name: product_list[index]['name'],
-          prod_picture: product_list[index]['picture'],
-          prod_old_price: product_list[index]['old_price'],
-          prod_price: product_list[index]['price']
-        );
+            prod_name: product_list[index]['name'],
+            prod_picture: product_list[index]['picture'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_price: product_list[index]['price']);
       },
     );
   }
@@ -98,35 +98,53 @@ class SingleProduct extends StatelessWidget {
   final prod_old_price;
   final prod_price;
 
-  SingleProduct({
-    this.prod_name,
-    this.prod_picture,
-    this.prod_old_price,
-    this.prod_price
-  });
+  SingleProduct(
+      {this.prod_name,
+      this.prod_picture,
+      this.prod_old_price,
+      this.prod_price});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Hero(tag: prod_name, child: Material(
-        child: InkWell(
-          onTap: (){},
-          child: GridTile(
-            footer: Container(
-              color: Colors.white70,
-              child: ListTile(
-                leading: Text(
-                  prod_name,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0),),
-                title: Text("Rp. $prod_price", style: TextStyle(color: Colors.red, fontSize: 12.0 , fontWeight: FontWeight.w800),),
-                subtitle: Text("Rp. $prod_old_price", style: TextStyle(color: Colors.black54, fontSize: 12.0 ,fontWeight: FontWeight.w800, decoration: TextDecoration.lineThrough),),
+      child: Hero(
+          tag: prod_name,
+          child: Material(
+            child: InkWell(
+              onTap: () {},
+              child: GridTile(
+                footer: Container(
+                  color: Colors.white70,
+                  child: ListTile(
+                    leading: Text(
+                      prod_name,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 12.0),
+                    ),
+                    title: Text(
+                      "Rp. $prod_price",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    subtitle: Text(
+                      "Rp. $prod_old_price",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w800,
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                  ),
+                ),
+                child: Image.asset(
+                  prod_picture,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            child: Image.asset(prod_picture, fit: BoxFit.cover,),
-          ),
-        ),
-      )),
+          )),
     );
   }
 }
-
